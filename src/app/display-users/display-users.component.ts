9a2394ddeec2efbb1e4b987a2189465b77b6ad22
AntouelaBitsa <antoubi10@gmail.com>
2024-11-30 16:41:09 +0200

    Display All Users page Completed

--- src/app/display-users/display-users.component.ts
+++ src/app/display-users/display-users.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ApiServiceService } from '../services/api-service.service';
+import { catchError } from 'rxjs';
+import { Users } from '../model/users.type';
 
 @Component({
   selector: 'app-display-users',
@@ -10,11 +12,23 @@ import { ApiServiceService } from '../services/api-service.service';
 })
 export class DisplayUsersComponent implements OnInit{
 
+  onUserClick(userDto: Users) {
+    const uId = encodeURIComponent(userDto.userID);
+    const url = `/user-details-tab?userId=${uId}`;  //maybe must change to get path variable or backend to receive query parameter
+    window.open(url, '_blank');
+  }
   userService = inject(ApiServiceService);
-  userSignal = signal<Array<{ firstName: string; lastName: string }>>([]);
+  userSignal = signal<Array<Users>>([]);
 
   ngOnInit(): void {
-    this.userSignal.set(this.userService.users);
-    console.log(this.userService.getUsers);
+    this.userService.getUsersFromAPI().
+    pipe(catchError((err) => {
+      console.log(err);
+      throw err;
+    })).
+    subscribe((users) => {
+      console.log(this.userSignal);
+      this.userSignal.set(users);
+    });
   }
 }
