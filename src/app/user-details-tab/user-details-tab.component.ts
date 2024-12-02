a74acfe9b1919dbb22dc99946fd5d4120403d8ba
AntouelaBitsa <antoubi10@gmail.com>
2024-12-02 21:20:09 +0200

    Refining code and fixing rendering issues from API response

--- src/app/user-details-tab/user-details-tab.component.ts
+++ src/app/user-details-tab/user-details-tab.component.ts
@@ -3,37 +3,59 @@ import { catchError, single } from 'rxjs';
 import { Users } from '../model/users.type';
 import { ActivatedRoute } from '@angular/router';
 import { ApiServiceService } from '../services/api-service.service';
+import { NonNullAssert } from '@angular/compiler';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-details-tab',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './user-details-tab.component.html',
   styleUrl: './user-details-tab.component.scss'
 })
 export class UserDetailsTabComponent implements OnInit{
+  // selectedUserId!: number;
   selectedUserId = signal<number|null>(null);
   userService = inject(ApiServiceService);
+  users: any;
   userObj = signal<Users|null>(null);
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(
-      (params) =>{
+    this.users = this.userObj();
+    this.users = {
+      homeAddress: ''
+    };
+
+
+    this.route.params.subscribe(
+      params => {
+        console.log('Route Params:', params);
         const userId = params['userId'] ? +params['userId'] : null;
+        console.log('userId Param:', userId);
         if(userId){
-          this.userService.getUserById(userId).pipe(catchError((err) => {
-            console.log(err);
-            throw err;
-          })).
-          subscribe((users) => {
-            console.log(this.userObj);
-            this.userObj.set(users);
+          this.userService.getUserById(userId).
+          // pipe(catchError((err) => {
+          //   console.log(err);
+          //   throw err;
+          // })).
+          subscribe((user: Users) => {
+            console.log('Response: ',user)
+            // this.userObj.set(users);
+            this.users = user;
+            console.log('User Object signal',this.users);
+            console.log('user home: '+ user.homeAdd.address);
+            console.log('user work: '+ user.workAdd.address);
+            // this.homeAddress = user?.homeAddress?.homeAddress;
+            // console.log('homeAddress Object signal',this.homeAddress);
           });
         }
       }
     );
+
   }
 
 }
+
+
