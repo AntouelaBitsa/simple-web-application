eb3ebee3998e0568c2376c0269ae630d54541335
AntouelaBitsa <antoubi10@gmail.com>
2024-12-02 21:15:03 +0200

    Implementation of endpoint calls via HTTP

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,23 +1,41 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Users } from '../model/users.type';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root' //this shoul be deleted when i am calling adding the service as a provider in a component or page
 })
 export class ApiServiceService {
-  // Dummy list of users
-  users: Array<{ firstName: string; lastName: string }> = [
-    { firstName: 'John', lastName: 'Doe' },
-    { firstName: 'Jane', lastName: 'Smith' },
-    { firstName: 'Alice', lastName: 'Johnson' },
-    { firstName: 'Bob', lastName: 'Brown' },
-    { firstName: 'Charlie', lastName: 'Davis' },
-  ]
 
-  // constructor() { }
+  apiConnector = inject(HttpClient);
+  // baseUrl = 'http://localhost:8080/';
 
-  // Method to get all users
-  getUsers() {
-    return this.users;
+  // Method to get all users from API
+  getUsersFromAPI(): Observable<Array<Users>> {
+    const endpoint = `http://localhost:8080/getUsersList`;
+    return this.apiConnector.get<Array<Users>>(endpoint);
+  }
+
+  getUserById(uId: number): Observable<Users>{
+    const endpoint = `http://localhost:8080/getUserById/${uId}`;
+    return this.apiConnector.get<Users>(endpoint).pipe(
+      map(users => ({
+        ...users,
+        homeAdd: {
+          id: users.homeAdd.homeAddID,
+          address: users.homeAdd.homeAddress,
+        },
+        workAdd: {
+          id: users.workAdd.workAddID,
+          address: users.workAdd.workAddress,
+        }
+      })));
+  }
+
+  registerUser(user: any): any{
+    const endpoint = `http://localhost:8080/registerUser`;
+    return this.apiConnector.post(endpoint, user);
   }
 
 }
